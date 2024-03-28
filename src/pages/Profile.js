0a39da17f28824ace171d58f655b03f0b3038237
BEAST0099 <aramis.facchinetti16@gmail.com>
2024-03-28 19:57:33 +0100

    started update user details

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,77 +1,97 @@
-import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function ProfilePage() {
-    //const [name, setName] = useState('John Doe'); 
-    //const [location, setLocation] = useState('Bay Area, San Francisco, CA'); 
-    const [website, setWebsite] = useState('https://bootdey.com'); 
-    const [github, setGithub] = useState('bootdey'); 
-    const [twitter, setTwitter] = useState('@bootdey'); 
-    const [instagram, setInstagram] = useState('bootdey'); 
-    const [facebook, setFacebook] = useState('bootdey'); 
-    const [email, setEmail] = useState('fip@jukmuh.al'); 
-    const [phone, setPhone] = useState('239-816-9029'); 
-    const [mobile, setMobile] = useState('320-380-4539'); 
-    const [address, setAddress] = useState('Bay Area, San Francisco, CA'); 
+    const accessToken = localStorage.getItem('accessToken');
 
     const Spinner = () => (
         <div className="spinner">User not found</div>
-      );
-    
-    const location = useLocation();
-    const agent = location.state ? location.state.agent : null;
-    console.log("agent " + JSON.stringify(agent));
+    );
 
     let content = <Spinner />;
 
-    if (agent) {
+    const [updateStatus, setUpdateStatus] = useState('');
+    const [userData, setUserData] = useState();
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setUserData({ ...userData, [name]: value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post('http://localhost:8005/updateUser', userData);
+            setUpdateStatus('success');
+        } catch (error) {
+            console.error('Error updating user data:', error);
+            setUpdateStatus('error');
+        }
+    };
+
+    useEffect(() => {
+        async function getUser() {
+            try {
+                let response = await axios.get('http://localhost:8005/user', { params: { accessToken: accessToken } });
+                console.log(response.data);
+                setUserData(response.data);
+            } catch (error) {
+                console.error('Error updating user data:', error);
+            }
+        }
+        getUser();
+    }, []);
+
+    if (userData) {
         content = (
             <div className="container mt-5">
-            {/* Profile widget */}
-            <div className="row gutters-sm">
-                <div className="col-md-4 mb-3">
-                    <div className="card">
-                        <div className="card-body">
-                            <div className="d-flex flex-column align-items-center text-center">
-                                <img src={agent.image} alt="Admin" className="rounded-circle" width="150"></img>
-                                <div className="mt-3">
-                                    <h4>{agent.name}</h4>
-                                    <p className="text-primary mb-1">Rating: {agent.rating}</p>
-                                    <p className="text-muted font-size-sm">ciao</p>
-                                    <button className="btn btn-primary me-1">Rate</button>
-                                    <button className="btn btn-outline-primary">Message</button>
+                {/* Profile widget */}
+                <div className="row gutters-sm">
+                    <div className="col-md-4 mb-3">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="d-flex flex-column align-items-center text-center">
+                                    <img src={userData.image} alt="Admin" className="rounded-circle" width="150"></img>
+                                    <div className="mt-3">
+                                        <h4>{userData.name}</h4>
+                                        <p className="text-primary mb-1">Rating: {userData.rating}</p>
+                                        <p className="text-muted font-size-sm">ciao</p>
+                                        <button className="btn btn-primary me-1">Rate</button>
+                                        <button className="btn btn-outline-primary">Message</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="card mt-3">
-                        <ul className="list-group list-group-flush">
-                            <SocialLink iconClass="feather feather-globe" title="Website" value={website} />
-                            <SocialLink iconClass="feather feather-github" title="Github" value={github} />
-                            <SocialLink iconClass="feather feather-twitter" title="Twitter" value={twitter} />
-                            <SocialLink iconClass="feather feather-instagram" title="Instagram" value={instagram} />
-                            <SocialLink iconClass="feather feather-facebook" title="Facebook" value={facebook} />
-                        </ul>
-                    </div>
-                </div>
-                <div className="col-md-8">
-                    <div className="card mb-3">
-                        <div className="card-body">
-                            <ProfileField title="Full Name" value={agent.name} />
-                            <ProfileField title="Email" value={agent.email} />
-                            <ProfileField title="Phone" value={agent.phone} />
-                            <ProfileField title="Mobile"  value={agent.phone} />
-                            <ProfileField title="Address" value={agent.adress} />
-                            <div className="row">
-                                <div className="col-sm-12">
-                                    <a className="btn btn-dark" target="__blank" href="https://www.bootdey.com/snippets/view/profile-edit-data-and-skills">Edit</a>
-                                </div>
+                    <div className="col-md-8">
+                        <div className="card mb-3">
+                            <div className="card-body">
+                                {updateStatus === 'success' && <div className="success-message">Update successful!</div>}
+                                {updateStatus === 'error' && <div className="error-message">Update failed. Please try again.</div>}
+                                <form onSubmit={handleSubmit}>
+                                    <div>
+                                        <label>Name:</label>
+                                        <input type="text" name="name" value={userData.name} onChange={handleChange} required />
+                                    </div>
+                                    <div>
+                                        <label>Email:</label>
+                                        <input type="email" name="email" value={userData.email} onChange={handleChange} required />
+                                    </div>
+                                    <div>
+                                        <label>Phone number:</label>
+                                        <input type="tel" name="phoneNumber" value={userData.phoneNumber} onChange={handleChange} required />
+                                    </div>
+                                    <div>
+                                        <label>Address:</label>
+                                        <input type="text" name="address" value={userData.address} onChange={handleChange} required />
+                                    </div>
+                                    <button type="submit">Update</button>
+                                </form>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
         );
     }
 
