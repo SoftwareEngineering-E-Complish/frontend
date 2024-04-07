d608897e27d61d231a6687611731c072a90a45ae
nataell.cornu <44674633+nataell95@users.noreply.github.com>
2024-04-07 13:48:23 +0200

    Feature/create listing (#10)
    * created the createAdd page
    
    * ip
    
    * send images to backend by clickin on submit button
    
    * adusted to match georges endpoint
    
    * fixing router
    
    * ip
    
    * changing fields such that they map with backedn fields
    
    * Refactor post request to use form data
    
    * Update location field name
    
    ---------
    
    Co-authored-by: Piotr <piotr.wolinski.2017@outlook.com>

--- src/components/routing/routers/AppRouter.js
+++ src/components/routing/routers/AppRouter.js
@@ -6,23 +6,35 @@ import ProductList from "../../../products/ProductList";
 import Login from "../../../pages/Login";
 import Register from "../../../pages/Register";
 import Profile from "../../../pages/Profile";
+import AddPage from "../../../pages/CreateAdd";
 import Callback from "../../../pages/Callback";
 import { LoggedInGuard } from "../routeProtectors/ProfileGuard.js";
 
 
 const AppRouter = () => {
+  //        <Route path="/createAdd" element={<ProfileGuard />}>
+  //<Route path="/createAdd" element = {<AddPage newCreated={true}/>}>
+  //</Route>
   return (
-    <Routes>
-      <Route path="/properties" element={<ProductList />} />
-      <Route path="/properties/:id" element={<ProductDetail />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/profile" element={<LoggedInGuard />}>
-        <Route path="/profile" element={<Profile />} />
-      </Route>
-      <Route path="/" element={<Landing />} />
-      <Route path="/callback" element={<Callback />} />
-    </Routes>
+      <Routes>
+        <Route path="/properties" element={<ProductList />}>
+        </Route>
+        <Route path="/properties/:id" element = {<ProductDetail />}>
+        </Route>
+        <Route path="/login" element = {<Login />}>
+        </Route>
+        <Route path="/register" element = {<Register />}>
+        </Route>
+        <Route path="/profile" element={<ProfileGuard />}>
+            <Route path="/profile" element = {<Profile />}/>
+        </Route>
+          <Route path="/createAdd" element = {<AddPage newCreated={true}/>}>
+        </Route>
+        <Route path="/" element = {<Landing />}>
+        </Route>
+        <Route path="/callback" element = {<Callback />}>
+        </Route>
+      </Routes>
   );
 };
 
